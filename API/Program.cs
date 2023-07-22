f2d8536fafb17ef7043352347c68b5db37b4f432
seoninja13 <dachevivo@gmail.com>
2023-07-22 11:00:31 -0700

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,9 +15,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
